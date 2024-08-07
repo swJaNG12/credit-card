2a2df7d7aa81dede5aea8f8edccce42b8068e460
swJaNG12 <cbdjej38483@gmail.com>
2024-08-07 20:03:47 +0900

    💄 emotion test
    - css props: String Styles
    - Styled Components
    - Composition

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
+import { css } from '@emotion/react'
+import styled from '@emotion/styled'
 import './App.css'
 
+const containerStyle = css`
+  background-color: blue;
+`
+
+const Button = styled.button`
+  background-color: red;
+  color: #fff;
+`
+
+const base = css`
+  ${containerStyle}
+  color: yellow;
+`
+
 function App() {
-  console.log(process.env)
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" css={containerStyle}>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Button>Example</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          css={base}
         >
           Learn React
         </a>
