6401eb8c4123170076fd19cdfbd1400cb69deb0c
swJaNG12 <cbdjej38483@gmail.com>
2024-08-08 14:26:39 +0900

    💄 button reset css 추가

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -128,6 +128,17 @@ const baseStyle = css`
     border-collapse: collapse;
     border-spacing: 0;
   }
+  button {
+    border: none;
+    margin: 0;
+    padding: 0;
+    width: auto;
+    overflow: visible;
+    background: transparent;
+    color: inherit;
+    font: inherit;
+    line-height: normal;
+  }
 `
 
 export default baseStyle
