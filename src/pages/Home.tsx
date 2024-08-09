6597268de7dcbdf1b868002488bbb710c02440ad
swJaNG12 <cbdjej38483@gmail.com>
2024-08-09 22:40:41 +0900

    🔧 Home 페이지, Test 페이지 추가

---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import { getCards } from '@/remote/card'
+import Top from '@shared/Top'
+import { useEffect } from 'react'
+
+export default function HomePage() {
+  useEffect(() => {
+    getCards().then((response) => console.log(response))
+  }, [])
+
+  return (
+    <div>
+      <Top
+        title="혜택 좋은 카드"
+        subTitle="회원님을 위해서 혜택 좋은 카드를 모아봤어요"
+      />
+    </div>
+  )
+}
---
+++ src/pages/Test.tsx
@@ -0,0 +1,10 @@
+import Button from '@/components/shared/Button'
+import CardListAddButton from '@/components/test/CardListAddButton'
+
+export default function TestPage() {
+  return (
+    <div>
+      <CardListAddButton />
+    </div>
+  )
+}
