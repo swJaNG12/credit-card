06906710161be7b12441f61827f0dcc6a836d984
swJaNG12 <cbdjej38483@gmail.com>
2024-08-09 22:35:17 +0900

    🔨 App.tsx: Home 페이지, Test 페이지 라우터 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,15 @@
-import { css } from '@emotion/react'
-import styled from '@emotion/styled'
-import './App.css'
-
-// css Prop(String Styles)
-const containerStyle = css`
-  background-color: blue;
-`
-
-// Styled Components
-const Button = styled.button`
-  background-color: red;
-  color: #fff;
-`
-
-// Composition
-const base = css`
-  ${containerStyle}
-  color: yellow;
-`
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from '@pages/Home'
+import TestPage from '@pages/Test'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header" css={containerStyle}>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Button>Example</Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          css={base}
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/test" element={<TestPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
-
 export default App
