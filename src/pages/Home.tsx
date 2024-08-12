1d56326e55ab23e1a32a56e18b97c6ea1bf5a418
swJaNG12 <cbdjej38483@gmail.com>
2024-08-12 11:26:33 +0900

    AdBanners  컴포넌트 테스트

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,13 @@
-import { getAdBanners } from '@/remote/adBanner'
-import { getCards } from '@/remote/card'
 import Top from '@shared/Top'
-import { useEffect } from 'react'
-
+import AdBanners from '@components/home/AdBanners'
 export default function HomePage() {
-  useEffect(() => {
-    getCards().then((response) => console.log(response))
-  }, [])
-  useEffect(() => {
-    getAdBanners().then((response) => console.log(response))
-  }, [])
-
   return (
     <div>
       <Top
         title="혜택 좋은 카드"
         subTitle="회원님을 위해서 혜택 좋은 카드를 모아봤어요"
       />
+      <AdBanners />
     </div>
   )
 }
