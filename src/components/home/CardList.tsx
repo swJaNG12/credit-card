e3797d0566101bb11100ab939de95599e0ae1d34
swJaNG12 <cbdjej38483@gmail.com>
2024-08-12 16:01:59 +0900

    ✨ 카드 데이터 무한 스크롤 적용
    - useInfiniteQuery
    - firestore limit, query, startAfter 적용

--- src/components/home/CardList.tsx
+++ src/components/home/CardList.tsx
@@ -1,20 +1,50 @@
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useInfiniteQuery } from '@tanstack/react-query'
 import { getCards } from '@remote/card'
 import ListRow from '@shared/ListRow'
+import { flatten } from 'lodash'
+import InfiniteScroll from 'react-infinite-scroll-component'
+import { useCallback } from 'react'
 
 export default function CardList() {
-  const { data: cards } = useQuery({
+  const {
+    data,
+    hasNextPage = false,
+    fetchNextPage,
+    isFetching,
+  } = useInfiniteQuery({
     queryKey: ['cards'],
-    queryFn: getCards,
+    queryFn: ({ pageParam }: any) => {
+      return getCards(pageParam)
+    },
+    initialPageParam: null,
+    getNextPageParam: (snapshot) => {
+      return snapshot.lastVisible
+    },
   })
 
-  if (cards === null) {
+  if (!data) {
     return null
   }
 
+  const loadMore = () => {
+    if (hasNextPage === false || isFetching) {
+      return
+    }
+    fetchNextPage()
+  }
+
+  console.log(hasNextPage)
+
+  const cards = flatten(data.pages.map(({ items }) => items))
+
   return (
     <div>
-      <ul>
+      <InfiniteScroll
+        dataLength={cards.length}
+        hasMore={hasNextPage}
+        loader={<div>Loading...</div>}
+        next={loadMore}
+      >
         {cards?.map((card, idx) => (
           <ListRow
             key={card.id}
@@ -32,7 +62,7 @@ export default function CardList() {
             withArrow={true}
           />
         ))}
-      </ul>
+      </InfiniteScroll>
     </div>
   )
 }
--- src/remote/card.ts
+++ src/remote/card.ts
@@ -1,12 +1,30 @@
-import { collection, getDocs } from 'firebase/firestore'
+import {
+  collection,
+  getDocs,
+  limit,
+  query,
+  startAfter,
+} from 'firebase/firestore'
 import { db } from '@remote/firebase'
 import { COLLECTIONS } from '@/constants'
 import { Card } from '@/models/card'
 
-export async function getCards() {
-  const querySnapShot = await getDocs(collection(db, COLLECTIONS.CARD))
-  return querySnapShot.docs.map((doc) => ({
+// pageParam 지금 보이고 있는 맨 마지막 요소
+export async function getCards(pageParam: any) {
+  const cardQuery = !pageParam
+    ? query(collection(db, COLLECTIONS.CARD), limit(10))
+    : query(collection(db, COLLECTIONS.CARD), startAfter(pageParam), limit(10))
+
+  const querySnapShot = await getDocs(cardQuery)
+
+  // 불러온 스냅샷의 맨 마지막 문서를 커서로 판단
+  const lastVisible = querySnapShot.docs[querySnapShot.docs.length - 1]
+
+  // 불러온 스냅샷 데이터
+  const items = querySnapShot.docs.map((doc) => ({
     id: doc.id,
     ...(doc.data() as Card),
   }))
+
+  return { items, lastVisible }
 }
