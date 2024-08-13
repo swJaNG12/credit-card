25714078a2a1a35af619df5d8e74e63eed45954b
swJaNG12 <cbdjej38483@gmail.com>
2024-08-14 01:37:36 +0900

    🔨 회원가입 완료 시 홈으로 이동

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -4,8 +4,11 @@ import { FormValues } from '@models/signup'
 import Form from '@components/signup/Form'
 import { collection, doc, setDoc } from 'firebase/firestore'
 import { COLLECTIONS } from '@/constants'
+import { useNavigate } from 'react-router-dom'
 
 export default function SignUpPage() {
+  const navigate = useNavigate()
+
   const handleSubmit = async (formValues: FormValues) => {
     const { email, password, name } = formValues
     const { user } = await createUserWithEmailAndPassword(auth, email, password)
@@ -21,6 +24,8 @@ export default function SignUpPage() {
     }
 
     await setDoc(doc(collection(db, COLLECTIONS.USER), user.uid), newUser)
+
+    navigate('/')
   }
 
   return (
