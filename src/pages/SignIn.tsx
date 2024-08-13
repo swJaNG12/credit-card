dc4952db639515e62083d7550f4d75aba5bb9e64
swJaNG12 <cbdjej38483@gmail.com>
2024-08-13 22:44:05 +0900

    ✨ 로그인 기능 구현
    - signInWithEmailAndPassword
    - UX를 위해 에러 종류에 따라 다른 내용의 alert 렌더링

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,11 +1,51 @@
+import { auth } from '@/remote/firebase'
 import Form from '@components/signin/Form'
 import { FormValues } from '@models/Signin'
+import { useAlertContext } from '@contexts/Alertcontext'
+
+import { signInWithEmailAndPassword } from 'firebase/auth'
 import { useCallback } from 'react'
+import { FirebaseError } from 'firebase/app'
+import { useNavigate } from 'react-router-dom'
 
 export default function SignInPage() {
-  const handleSubmit = useCallback((formValues: FormValues) => {
-    console.log(formValues)
-  }, [])
+  const { open } = useAlertContext()
+  const navigate = useNavigate()
+
+  const handleSubmit = useCallback(
+    async (formValues: FormValues) => {
+      const { email, password } = formValues
+
+      try {
+        const user = await signInWithEmailAndPassword(auth, email, password)
+
+        console.log('signinPage', user)
+
+        navigate('/')
+      } catch (error) {
+        // firebase 에러
+        if (error instanceof FirebaseError) {
+          open({
+            title: '계정 정보를 다시 확인해주세요',
+            onButtonClick: () => {
+              //
+            },
+          })
+
+          return
+        }
+        // 일반적인 에러
+        console.log(error)
+        open({
+          title: '잠시 후 다시 시도해주세요',
+          onButtonClick: () => {
+            //
+          },
+        })
+      }
+    },
+    [open],
+  )
 
   return (
     <div>
