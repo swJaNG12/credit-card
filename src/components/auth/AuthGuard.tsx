0ef2eaf731b0269daf27050b08d5613ecc987eaa
swJaNG12 <cbdjej38483@gmail.com>
2024-08-14 00:45:28 +0900

    🔧 AuthGoard: userAtom 값 설정
    - 로그인한 경우(유저 정보가 있는 경우): user 정보를 userAtom 값에 설정
    - 유저 정보가 없는 경우: userAtom 값 null로 설정

--- src/components/auth/AuthGuard.tsx
+++ src/components/auth/AuthGuard.tsx
@@ -1,15 +1,29 @@
 import { auth } from '@remote/firebase'
+import { userAtom } from '@atoms/user'
+import { User } from '@models/user'
+
 import { onAuthStateChanged } from 'firebase/auth'
 import React, { useState } from 'react'
+import { useSetRecoilState } from 'recoil'
 
 // 인증 처리, 인증에 대한 검사
 export default function AuthGuard({ children }: { children: React.ReactNode }) {
   const [initialize, setInitialize] = useState(false)
+  const setUserState = useSetRecoilState(userAtom)
 
   onAuthStateChanged(auth, (user) => {
+    console.log('auth gauard', user)
+    if (user !== null) {
+      setUserState({
+        uid: user.uid,
+        email: user.email,
+        displayName: user.displayName,
+      } as User)
+    } else {
+      setUserState(null)
+    }
     setInitialize(true)
   })
-
   if (initialize === false) {
     return null
   }
