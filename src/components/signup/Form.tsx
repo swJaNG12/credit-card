b3f916296f531ad037bab66f4bb7496237e1355e
swJaNG12 <cbdjej38483@gmail.com>
2024-08-13 11:55:01 +0900

    🚑 Form 컴포넌트 수정
    - Spacing 추가

--- src/components/signup/Form.tsx
+++ src/components/signup/Form.tsx
@@ -2,16 +2,20 @@ import Flex from '@shared/Flex'
 import TextField from '@shared/TextField'
 import FixedBottomButton from '@shared/FixedBottomButton'
 import { css } from '@emotion/react'
+import Spacing from '@shared/Spacing'
 
 export default function Form() {
   return (
     <Flex direction="column" css={formContainerStyles}>
       <TextField label="이메일" placeholder="이메일" />
+      <Spacing size={16} />
       <TextField label="패스워드" type="password" />
+      <Spacing size={16} />
       <TextField label="패스워드 재확인" type="password" />
+      <Spacing size={16} />
       <TextField label="이름" placeholder="이름" />
 
-      <FixedBottomButton label="회원가입" onClick={() => {}} />
+      <FixedBottomButton label="회원가입" onClick={() => {}} disabled={false} />
     </Flex>
   )
 }
