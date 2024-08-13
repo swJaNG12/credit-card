7b251540eab23e49553c962facb80df8b6f2868d
swJaNG12 <cbdjej38483@gmail.com>
2024-08-13 19:16:04 +0900

    🔨 signin form 작업

---
+++ src/components/signin/Form.tsx
@@ -0,0 +1,97 @@
+import Flex from '@shared/Flex'
+import TextField from '@shared/TextField'
+import Button from '@shared/Button'
+import Spacing from '@shared/Spacing'
+import Text from '@shared/Text'
+
+import { FormValues } from '@models/Signin'
+import { colors } from '@styles/colorPalette'
+
+import React, { useCallback, useMemo, useState } from 'react'
+import { css } from '@emotion/react'
+import { Link } from 'react-router-dom'
+import { isEmail } from 'validator'
+
+interface FormProps {
+  onSubmit: (formValues: FormValues) => void
+}
+
+export default function Form({ onSubmit }: FormProps) {
+  const [formValues, setFormValues] = useState<FormValues>({
+    email: '',
+    password: '',
+  })
+
+  const handleFormValues = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setFormValues((prevFormValues) => ({
+        ...prevFormValues,
+        [e.target.name]: e.target.value,
+      }))
+    },
+    [],
+  )
+
+  const errors = useMemo(() => validate(formValues), [formValues])
+
+  const isFormValid = Object.keys(errors).length === 0
+
+  return (
+    <Flex direction="column" css={FromContainerStyles}>
+      <TextField
+        label="이메일"
+        name="email"
+        onChange={handleFormValues}
+        placeholder="이메일"
+        value={formValues.email}
+      />
+      <Spacing size={16} />
+      <TextField
+        label="패스워드"
+        name="password"
+        onChange={handleFormValues}
+        type="password"
+        value={formValues.password}
+      />
+      <Spacing size={32} />
+      <Button
+        size="medium"
+        disabled={!isFormValid}
+        onClick={() => onSubmit(formValues)}
+      >
+        로그인
+      </Button>
+      <Spacing size={12} />
+      <Link to="/signup" css={LinkStyles}>
+        <Text typography="t6">아직 계정이 없으신가요?</Text>
+      </Link>
+    </Flex>
+  )
+}
+
+const LinkStyles = css`
+  text-align: center;
+
+  & > span:hover {
+    transition: color 0.2s;
+    color: ${colors.blue};
+  }
+`
+
+const FromContainerStyles = css`
+  padding: 24px;
+`
+
+function validate(formValues: FormValues) {
+  let errors: Partial<FormValues> = {}
+
+  if (isEmail(formValues.email) === false) {
+    errors.email = 'true'
+  }
+
+  if (formValues.password.length < 8) {
+    errors.password = 'true'
+  }
+
+  return errors
+}
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,5 +1,15 @@
-import React from 'react'
+import Form from '@components/signin/Form'
+import { FormValues } from '@models/Signin'
+import { useCallback } from 'react'
 
 export default function SignInPage() {
-  return <div>SignIn</div>
+  const handleSubmit = useCallback((formValues: FormValues) => {
+    console.log(formValues)
+  }, [])
+
+  return (
+    <div>
+      <Form onSubmit={handleSubmit} />
+    </div>
+  )
 }
