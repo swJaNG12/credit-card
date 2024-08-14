6763302731b52d7c57136b69273e483e06081b52
swJaNG12 <cbdjej38483@gmail.com>
2024-08-14 20:32:59 +0900

    ✅ Terms.tsx: Agreement 컴포넌트 테스트

--- src/components/apply/Terms.tsx
+++ src/components/apply/Terms.tsx
@@ -1,5 +1,38 @@
-import React from 'react'
+import Agreement from '@shared/Agreement'
 
 export default function Terms() {
-  return <div>Terms</div>
+  return (
+    <div>
+      <Agreement>
+        <Agreement.Title
+          checked={true}
+          onChange={(e, checked) => {
+            console.log(e)
+            console.log(checked)
+          }}
+        >
+          약관에 모두 동의
+        </Agreement.Title>
+        <Agreement.Description
+          checked={false}
+          onChange={(e, checked) => {
+            console.log(e)
+            console.log(checked)
+          }}
+        >
+          카드신청 관련 안내 및 필수/선택 동의
+        </Agreement.Description>
+        <Agreement.Description
+          checked={true}
+          onChange={(e, checked) => {
+            console.log(e)
+            console.log(checked)
+          }}
+          link="google.com"
+        >
+          (팔수) 개인(신용)정보 요약동의서
+        </Agreement.Description>
+      </Agreement>
+    </div>
+  )
 }
