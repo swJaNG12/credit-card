45f22d5cdb32a481555886ee97d4a758867f636c
swJaNG12 <cbdjej38483@gmail.com>
2024-08-14 23:37:47 +0900

    ✨ Terms 컴포넌트: 약관 동의, 약관 전체 동의 기능 구현
    - 전체 동의를 누르면 모든 약관이 '모두 동의' 또는 '모두 동의 취소' 처리된다.
    - 약관을 개별적으로도 동의할 수 있다.
    - isAllTermsAgreed 변수: 모든 약관이 동의되었는지 여부
    - handleAllAgreement 함수: 모든 약관 동의 여부 제어

--- src/components/apply/Terms.tsx
+++ src/components/apply/Terms.tsx
@@ -1,15 +1,46 @@
+import { useState } from 'react'
 import Agreement from '@shared/Agreement'
+
 import { TermsList } from '@constants/apply'
 
 export default function Terms() {
+  // useState(fn) => fn의 역할은 termsAgreements 초기값을 생성하는 것
+  // 초기값은 {termId1: boolean, termId2: boolean, ...} 이런 형태다.
+  const [termsAgreement, setTermsAgreement] = useState(() => {
+    return TermsList.reduce<Record<string, boolean>>(
+      (prev, term) => ({
+        ...prev,
+        [term.id]: false,
+      }),
+      {},
+    )
+  })
+
+  console.log(termsAgreement)
+
+  const handleAllAgreement = (checked: boolean) => {
+    const result = Object.keys(termsAgreement).reduce(
+      (prev, termId) => ({
+        ...prev,
+        [termId]: checked,
+      }),
+      {},
+    )
+
+    setTermsAgreement(result)
+  }
+
+  const isAllTermsAgreed = Object.values(termsAgreement).every(
+    (isTermAgreed) => isTermAgreed,
+  )
+
   return (
     <div>
       <Agreement>
         <Agreement.Title
-          checked={true}
-          onChange={(e, checked) => {
-            console.log(e)
-            console.log(checked)
+          checked={isAllTermsAgreed}
+          onChange={(_, checked) => {
+            handleAllAgreement(checked)
           }}
         >
           약관에 모두 동의
@@ -17,11 +48,13 @@ export default function Terms() {
         {TermsList.map((term, idx) => (
           <Agreement.Description
             key={term.id}
-            checked={false}
+            checked={termsAgreement[term.id]}
             link={term.link}
-            onChange={(e, checked) => {
-              console.log(e)
-              console.log(checked)
+            onChange={(_, checked) => {
+              setTermsAgreement((prevTerms) => ({
+                ...prevTerms,
+                [term.id]: checked,
+              }))
             }}
           >
             {term.title}
