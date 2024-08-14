1ca44f704581170dcb5cdb5cbe88856183151595
swJaNG12 <cbdjej38483@gmail.com>
2024-08-14 21:10:45 +0900

    🚑 Terms.tsx: AgreementDescription mock data로 맵핑

--- src/components/apply/Terms.tsx
+++ src/components/apply/Terms.tsx
@@ -1,4 +1,5 @@
 import Agreement from '@shared/Agreement'
+import { TermsList } from '@constants/apply'
 
 export default function Terms() {
   return (
@@ -13,25 +14,19 @@ export default function Terms() {
         >
           약관에 모두 동의
         </Agreement.Title>
-        <Agreement.Description
-          checked={false}
-          onChange={(e, checked) => {
-            console.log(e)
-            console.log(checked)
-          }}
-        >
-          카드신청 관련 안내 및 필수/선택 동의
-        </Agreement.Description>
-        <Agreement.Description
-          checked={true}
-          onChange={(e, checked) => {
-            console.log(e)
-            console.log(checked)
-          }}
-          link="google.com"
-        >
-          (팔수) 개인(신용)정보 요약동의서
-        </Agreement.Description>
+        {TermsList.map((term, idx) => (
+          <Agreement.Description
+            key={term.id}
+            checked={false}
+            link={term.link}
+            onChange={(e, checked) => {
+              console.log(e)
+              console.log(checked)
+            }}
+          >
+            {term.title}
+          </Agreement.Description>
+        ))}
       </Agreement>
     </div>
   )
