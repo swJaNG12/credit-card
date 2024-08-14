7e7718c5e41fab48c9bfa9461be217b014d2e89c
swJaNG12 <cbdjej38483@gmail.com>
2024-08-14 19:17:48 +0900

    🔨 카드 신청 폼 컴포넌트 작성
    - Apply 컴포넌트: 부모 컴포넌트, 전체 데이터 관리
    - Terms 컴포넌트: 약관 동의 컴포넌트
    - BasicInfo 컴포넌트: 기본정보 입력 컴포넌트
    -CardInfo 컴포넌트: 카드 정보 컴포넌트

--- src/pages/Apply.tsx
+++ src/pages/Apply.tsx
@@ -1,3 +1,16 @@
+import { useState } from 'react'
+import Terms from '@components/apply/Terms'
+import BasicInfo from '@components/apply/BasicInfo'
+import CardInfo from '@components/apply/CardInfo'
+
 export default function ApplyPage() {
-  return <div>Apply</div>
+  const [step, setStep] = useState<Number>(0)
+
+  return (
+    <div>
+      {step === 0 && <Terms />}
+      {step === 1 && <BasicInfo />}
+      {step === 2 && <CardInfo />}
+    </div>
+  )
 }
