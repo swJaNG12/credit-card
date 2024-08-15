51d5becb0907ea4e8aeb6b59d65cc9e275d19af9
swJaNG12 <cbdjej38483@gmail.com>
2024-08-15 15:04:14 +0900

    🪄 Button 컴포넌트 확장
    - 'ButtonGroup 컴포넌트 속성' 추가

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -7,6 +7,9 @@ import {
   buttonToggleMap,
   buttonSizeMap,
 } from '@styles/button'
+import React from 'react'
+import Flex from './Flex'
+import Text from './Text'
 
 interface ButtonProps {
   color?: ButtonColorType
@@ -16,7 +19,7 @@ interface ButtonProps {
   disabled?: boolean
 }
 
-const Button = styled.button<ButtonProps>(
+const BaseButton = styled.button<ButtonProps>(
   {
     cursor: 'pointer',
     fontWeight: 'bold',
@@ -41,5 +44,34 @@ const Button = styled.button<ButtonProps>(
         `
       : undefined,
 )
+
+export function ButtonGroup({
+  title,
+  children,
+}: {
+  title?: string
+  children: React.ReactNode
+}) {
+  return (
+    <Flex>
+      {title && <Text>{title}</Text>}
+      <Flex css={ButtonGroupStyle}>{children}</Flex>
+    </Flex>
+  )
+}
+
+const ButtonGroupStyle = css`
+  // flex-wrap: wrap;
+  gap: 10px;
+
+  & button {
+    flex: 1;
+  }
+`
+
+const Button = BaseButton as typeof BaseButton & {
+  Group: typeof ButtonGroup
+}
+Button.Group = ButtonGroup
 
 export default Button
