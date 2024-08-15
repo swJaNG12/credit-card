1fab23557343d7e6961fde27b4e4ef10d6ecfdbf
swJaNG12 <cbdjej38483@gmail.com>
2024-08-15 15:25:33 +0900

    🔨 CardInfo 컴포넌트 작업
    - Button Group 추가

--- src/components/apply/CardInfo.tsx
+++ src/components/apply/CardInfo.tsx
@@ -1,5 +1,20 @@
-import React from 'react'
+import Button from '@shared/Button'
 
 export default function CardInfo() {
-  return <div>CardInfo</div>
+  return (
+    <div>
+      <Button.Group title="해외 결제">
+        <Button size="medium">Master</Button>
+        <Button size="medium">국내 전용</Button>
+      </Button.Group>
+      <Button.Group title="후불교통기능">
+        <Button size="medium">신청안함</Button>
+        <Button size="medium">신청</Button>
+      </Button.Group>
+      <Button.Group title="후불하이패스카드">
+        <Button size="medium">신청안함</Button>
+        <Button size="medium">신청</Button>
+      </Button.Group>
+    </div>
+  )
 }
