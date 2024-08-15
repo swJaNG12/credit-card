d8b3ed456fb05736a1d5e5d8a28a2bf058c3e917
swJaNG12 <cbdjej38483@gmail.com>
2024-08-15 09:24:56 +0900

    🔧 apply.ts: 기본정보 옵션 mock data 설정, 타입 정의

--- src/constants/apply.ts
+++ src/constants/apply.ts
@@ -1,5 +1,6 @@
-import { Term } from '@models/apply'
+import { Option, Term } from '@models/apply'
 
+// 약관 목록
 export const TermsList: Term[] = [
   {
     id: '01',
@@ -10,4 +11,23 @@ export const TermsList: Term[] = [
     title: '(필수) 개인(신용)정보 요약동의서',
     link: 'https://www.google.com',
   },
+]
+
+// 연소득 옵션
+export const anuallIncomeOptions: Option[] = [
+  { label: '600만원 ~ 5,000만원', value: '600만원 ~ 5,000만원' },
+  { label: '5,000만원 ~ 1억원', value: '5,000만원 ~ 1억원' },
+  { label: '1억원 초과', value: '1억원 초과' },
+]
+
+// 신용점수 옵션
+export const creditScore: Option[] = [
+  { label: '600점 이상', value: '600점 이상' },
+  { label: '600점 미만', value: '600점 미만' },
+]
+
+// 결제일 옵션
+export const paymentDateOptions: Option[] = [
+  { label: '1일', value: '1일' },
+  { label: '25일', value: '25일' },
 ]
--- src/models/apply.ts
+++ src/models/apply.ts
@@ -14,3 +14,8 @@ export interface ApplyValues {
   appliedAt: Date
   cardId: string
 }
+
+export interface Option {
+  label: string
+  value: string | number | undefined
+}
