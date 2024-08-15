048d5c031cb1701a8b4204d91f1a03e1b9b7fa92
swJaNG12 <cbdjej38483@gmail.com>
2024-08-15 14:24:37 +0900

    ✨ BasicInfo 컴포넌트: 개인정보 선택 기능 구현
    - Terms 컴포넌트 로직과 유사한 로직 사용

--- src/components/apply/BasicInfo.tsx
+++ src/components/apply/BasicInfo.tsx
@@ -1,5 +1,72 @@
-import React from 'react'
+import { ChangeEvent, useState } from 'react'
 
-export default function BasicInfo() {
-  return <div>BasicInfo</div>
+import Select from '@shared/Select'
+import FixedBottomButton from '@shared/FixedBottomButton'
+import {
+  anuallIncomeOptions,
+  creditScore,
+  paymentDateOptions,
+} from '@constants/apply'
+import { ApplyValues } from '@models/apply'
+
+type BasicInfoValues = Pick<ApplyValues, 'salary' | 'creditScore' | 'payDate'>
+
+export default function BasicInfo({
+  onNext,
+}: {
+  onNext: (basicInfoValues: BasicInfoValues) => void
+}) {
+  const [basicInfoValues, setBasicInfoValues] = useState<BasicInfoValues>({
+    salary: '',
+    creditScore: '',
+    payDate: '',
+  })
+
+  const handleInfoChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setBasicInfoValues((prevInfo) => ({
+      ...prevInfo,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const isAllInfoSelected = Object.values(basicInfoValues).every(
+    (info) => info !== '',
+  )
+
+  // console.log(basicInfoValues)
+  // console.log(isAllInfoSelected)
+
+  return (
+    <div>
+      <Select
+        label="연소득"
+        name="salary"
+        options={anuallIncomeOptions}
+        placeholder={anuallIncomeOptions[0].label}
+        value={basicInfoValues.salary}
+        onChange={handleInfoChange}
+      />
+      <Select
+        label="신용점수"
+        name="creditScore"
+        options={creditScore}
+        placeholder={creditScore[0].label}
+        value={basicInfoValues.creditScore}
+        onChange={handleInfoChange}
+      />
+      <Select
+        label="결제일"
+        name="payDate"
+        options={paymentDateOptions}
+        placeholder={paymentDateOptions[0].label}
+        value={basicInfoValues.payDate}
+        onChange={handleInfoChange}
+      />
+      <FixedBottomButton
+        label="다음"
+        disabled={!isAllInfoSelected}
+        onClick={() => onNext(basicInfoValues)}
+      />
+    </div>
+  )
 }
--- src/pages/Apply.tsx
+++ src/pages/Apply.tsx
@@ -2,18 +2,24 @@ import { useState } from 'react'
 import Terms from '@components/apply/Terms'
 import BasicInfo from '@components/apply/BasicInfo'
 import CardInfo from '@components/apply/CardInfo'
+import { ApplyValues } from '@models/apply'
+
+type BasicInfoValues = Pick<ApplyValues, 'salary' | 'creditScore' | 'payDate'>
 
 export default function ApplyPage() {
-  const [step, setStep] = useState<Number>(0)
+  const [step, setStep] = useState<Number>(1)
 
   const handleTermsChange = (terms: string[]) => {
     console.log('terms', terms)
+  }
+  const handleBasicInfoChage = (basicInfoValues: BasicInfoValues) => {
+    console.log('info', basicInfoValues)
   }
 
   return (
     <div>
       {step === 0 && <Terms onNext={handleTermsChange} />}
-      {step === 1 && <BasicInfo />}
+      {step === 1 && <BasicInfo onNext={handleBasicInfoChage} />}
       {step === 2 && <CardInfo />}
     </div>
   )
