f97b55748423296e5e07b47e6d222557c93793e8
swJaNG12 <cbdjej38483@gmail.com>
2024-08-16 18:08:48 +0900

    🪄 Create updateApplyCard 함수: 신청된 카드 정보를 업데이트

--- src/remote/apply.ts
+++ src/remote/apply.ts
@@ -1,4 +1,11 @@
-import { addDoc, collection } from 'firebase/firestore'
+import {
+  addDoc,
+  collection,
+  getDocs,
+  query,
+  updateDoc,
+  where,
+} from 'firebase/firestore'
 import { db } from './firebase'
 
 import { COLLECTIONS } from '@constants'
@@ -6,4 +13,27 @@ import { ApplyValues } from '@models/apply'
 
 export async function applyCard(applyValues: ApplyValues) {
   return await addDoc(collection(db, COLLECTIONS.CARD_APPLY), applyValues)
+}
+
+// 카드를 찾고 그 카드의 정보를 업데이트
+export async function updateApplyCard({
+  cardId,
+  userId,
+  applyValues,
+}: {
+  cardId: string
+  userId: string
+  applyValues: Partial<ApplyValues>
+}) {
+  const snapshot = await getDocs(
+    query(
+      collection(db, COLLECTIONS.CARD_APPLY),
+      where('userId', '==', userId),
+      where('cardId', '==', cardId),
+    ),
+  )
+
+  const [applied] = snapshot.docs
+
+  updateDoc(applied.ref, applyValues)
 }
