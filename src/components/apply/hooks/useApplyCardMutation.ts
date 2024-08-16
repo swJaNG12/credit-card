88178dac350b003cc7f323064e796acba73f655e
swJaNG12 <cbdjej38483@gmail.com>
2024-08-16 13:38:23 +0900

    🪄 useApplyCardMutation 커스텀 훅 사용성 확장
    - onSuccess, onError를 받아 실행
    - 성공했을 때와 실패했을 때의 동작을 사용처에 넘겨 다양한 상황에 대응가능

--- src/components/apply/hooks/useApplyCardMutation.ts
+++ src/components/apply/hooks/useApplyCardMutation.ts
@@ -2,8 +2,22 @@ import { ApplyValues } from '@models/apply'
 import { applyCard } from '@remote/apply'
 import { useMutation } from '@tanstack/react-query'
 
-export default function useApplyCardMutation() {
+interface useApplyCardMutationProps {
+  onSuccess: () => void
+  onError: () => void
+}
+
+export default function useApplyCardMutation({
+  onSuccess,
+  onError,
+}: useApplyCardMutationProps) {
   return useMutation({
     mutationFn: (applyValues: ApplyValues) => applyCard(applyValues),
+    onSuccess: () => {
+      onSuccess()
+    },
+    onError: () => {
+      onError()
+    },
   })
 }
--- src/pages/Apply.tsx
+++ src/pages/Apply.tsx
@@ -2,6 +2,9 @@ import Apply from '@components/apply'
 import useApplyCardMutation from '@components/apply/hooks/useApplyCardMutation'
 
 export default function ApplyPage() {
-  const { mutate } = useApplyCardMutation()
+  const { mutate } = useApplyCardMutation({
+    onSuccess: () => {},
+    onError: () => {},
+  })
   return <Apply onSubmit={mutate} />
 }
