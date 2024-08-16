8539875f66b48aca2dc74539665b3a7e74915b80
swJaNG12 <cbdjej38483@gmail.com>
2024-08-16 18:07:46 +0900

    🪄 카드사 mock 함수 작성
    - getApplyStatus 함수: 랜덤으로 상태값 전달
    - isSuccess, isError 대신, 랜덤으로 받아오는 데이터의 status 값으로 성공, 실패 분기 처리

--- src/components/apply/hooks/usePollApplyStatus.ts
+++ src/components/apply/hooks/usePollApplyStatus.ts
@@ -1,3 +1,4 @@
+import { APPLY_STATUS } from '@/models/apply'
 import { useQuery } from '@tanstack/react-query'
 
 interface usePollApplyStatusProps {
@@ -9,9 +10,27 @@ export default function usePollApplyStatus({
 }: usePollApplyStatusProps) {
   return useQuery({
     queryKey: ['applyStatus'],
-    queryFn: () => {},
+    queryFn: getApplyStatus,
     enabled,
     refetchInterval: 2000,
     staleTime: 0,
   })
+}
+
+// 카드사 mocking 함수, 랜덤하게 상태값 전달
+function getApplyStatus() {
+  const values = [
+    APPLY_STATUS.READY,
+    APPLY_STATUS.PROGRESS,
+    APPLY_STATUS.COMPLETE,
+    APPLY_STATUS.REJECT,
+  ]
+
+  const status = values[Math.floor(Math.random() * values.length)]
+
+  if (status === APPLY_STATUS.REJECT) {
+    return { status: 'error', message: '카드 발급에 실패했습니다.' }
+  }
+
+  return { status }
 }
--- src/pages/Apply.tsx
+++ src/pages/Apply.tsx
@@ -1,10 +1,25 @@
+import usePollApplyStatus from '@/components/apply/hooks/usePollApplyStatus'
+import { APPLY_STATUS } from '@/models/apply'
 import Apply from '@components/apply'
 import useApplyCardMutation from '@components/apply/hooks/useApplyCardMutation'
+import { useState } from 'react'
 
 export default function ApplyPage() {
+  const [readyToPoll, setReadyToPoll] = useState(false)
+
+  const { data } = usePollApplyStatus({
+    enabled: readyToPoll,
+  })
+
+  if (data?.status === APPLY_STATUS.COMPLETE) {
+    console.log('성공')
+  } else if (data?.status === 'error') {
+    console.log('실패')
+  }
+
   const { mutate } = useApplyCardMutation({
     onSuccess: () => {
-      console.log('카드 추가~')
+      setReadyToPoll(true)
     },
     onError: () => {
       window.history.back()
