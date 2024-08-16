7d31e303d3be14c1c65bf71808823537e2ff4b34
swJaNG12 <cbdjej38483@gmail.com>
2024-08-16 20:10:43 +0900

    ✨ cardApplyFilnal 함수 작성
    - 카드 신청 결과에 따라 신청 카드의 status를 업데이트
    - 업데이트 완료 후 ApplyDone 페이지로 이동

--- src/pages/Apply.tsx
+++ src/pages/Apply.tsx
@@ -1,23 +1,53 @@
 import usePollApplyStatus from '@/components/apply/hooks/usePollApplyStatus'
-import { APPLY_STATUS } from '@/models/apply'
+import useUser from '@/hooks/auth/useUser'
+import { ApplyValues, APPLY_STATUS } from '@/models/apply'
+import { updateApplyCard } from '@/remote/apply'
 import Apply from '@components/apply'
 import useApplyCardMutation from '@components/apply/hooks/useApplyCardMutation'
 import { useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 
 export default function ApplyPage() {
+  const user = useUser()
+  const { id } = useParams()
+  const navigate = useNavigate()
   const [readyToPoll, setReadyToPoll] = useState(false)
 
   const { data } = usePollApplyStatus({
     enabled: readyToPoll,
   })
 
-  if (data?.status === APPLY_STATUS.COMPLETE) {
-    console.log('성공')
-  } else if (data?.status === 'error') {
-    console.log('실패')
+  const cardApplyFilnal = async (status: string) => {
+    if (status === APPLY_STATUS.COMPLETE) {
+      console.log('성공')
+      await updateApplyCard({
+        userId: user?.uid as string,
+        cardId: id as string,
+        applyValues: {
+          status: APPLY_STATUS.COMPLETE,
+        } as Partial<ApplyValues>,
+      })
+      navigate('/apply/done?success=true')
+    } else if (status === APPLY_STATUS.REJECT) {
+      console.log('실패')
+      await updateApplyCard({
+        userId: user?.uid as string,
+        cardId: id as string,
+        applyValues: {
+          status: APPLY_STATUS.REJECT,
+        } as Partial<ApplyValues>,
+      })
+      navigate('/apply/done?success=false')
+    }
   }
 
-  const { mutate } = useApplyCardMutation({
+  if (data?.status === APPLY_STATUS.COMPLETE) {
+    cardApplyFilnal(data?.status)
+  } else if (data?.status === 'error') {
+    cardApplyFilnal(data?.status)
+  }
+
+  const { mutate, status } = useApplyCardMutation({
     onSuccess: () => {
       setReadyToPoll(true)
     },
@@ -25,5 +55,10 @@ export default function ApplyPage() {
       window.history.back()
     },
   })
+
+  if (readyToPoll || status === 'pending') {
+    return <div>Loading...</div>
+  }
+
   return <Apply onSubmit={mutate} />
 }
